b9143c170b6ce1b6cb7f830d96d20221d3ca24b4
Luiz Neto <98713773+Netogt@users.noreply.github.com>
2022-04-18 19:44:14 -0300

    site final

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
-let fundofor = document.getElementById('fundfor')
+//variaveis
+
+var fundofor = document.getElementById('fundfor')
+var noteErro = document.getElementById('erroEm')
+
+//functions
+
+function emailEr(){
+    noteErro.style.display = 'block'
+    noteErro.innerHTML = 'Os campos de E-mail não estão iguais'
+    
+}
+function emailCd(){
+    noteErro.innerHTML = 'Esse endereço de E-mail já foi cadastrado'
+    noteErro.style.display = 'block'
+}
+
 
 function abrinsc(){
     fundofor.style.display = "block"
+    
 }
+
 function voltarhome(){
     fundofor.style.display = "none"
 }
